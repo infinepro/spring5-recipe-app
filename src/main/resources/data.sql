fd41ae20cc6535c594e34b7a8c5cb6abbc9bea80
maksimka <mickey_6@mail.ru>
2020-04-15 11:34:13 +0300

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO category(description)
+VALUES ('American'), ('Italian'), ('Mexican'), ('Fast food');
+
+INSERT INTO unit_of_measure(uom)
+VALUES ('teaspoon'), ('tablespoon'), ('cup'), ('pinch'), ('ounce');
