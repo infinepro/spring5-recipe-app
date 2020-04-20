81fa0ee9e97ee2377529a3b15c4f0ebadfba1ca5
maksimka <mickey_6@mail.ru>
2020-04-20 15:02:29 +0300

    tests ok

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -7,37 +7,43 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.configuration.MockAnnotationProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 //@ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = IndexController.class)
+//@WebMvcTest(controllers = IndexController.class)
 class IndexControllerTest {
 
+    @Mock
+    RecipeRepository recipeRepository;
+
+    IndexController indexController;
     RecipeService recipeService;
-
-    //@Mock
-    //RecipeRepository recipeRepository;
-
-    @Autowired
-    MockMvc mockMvc;
-
     String indexView;
+
 
     @BeforeEach
     void setUp() {
-      //  recipeService = new RecipeServiceImpl(recipeRepository);
-        //indexView = "index";
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+        indexController = new IndexController(recipeService);
+
+        indexView = "index";
     }
 
     @Test
     void getIndexPage() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name(indexView));
