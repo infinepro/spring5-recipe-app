5ee28704f3fc2403aec164bbf23a18adaaa6f92e
maksimka <mickey_6@mail.ru>
2020-04-24 21:22:36 +0300

    add new recipe (without ingredient, category)

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,11 +1,13 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -21,5 +23,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.getRecipeById(id));
 
         return "recipe/show";
+    }
+
+    @GetMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/add-recipe-form";
+    }
+
+
+    @PostMapping("recipe/new")
+    public String saveOrUpdateNewRecipe(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand saveRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + saveRecipeCommand.getId();
     }
 }
